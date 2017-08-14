a5b7f74fd754e057978d1f97e6ab0afb78ff17f9
0raclewind <look4award@yahoo.com>
2017-08-14 17:39:05 +0100

    added scrolling

--- portfolio/js/jquery.js
+++ portfolio/js/jquery.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  $("body").on('click','#mainNavBar', function(){ $(this).collapse("toggle"); });  
+  $("body").on('click','#mainNavBar', function(){ $(this).collapse("toggle"); });
+  setBindings();
 });
+
+function setBindings() {
+  var scrollLink = $(".scroll")
+
+  //Smooth scrolling
+  scrollLink.click(function(e){
+    e.preventDefault();
+    $("body, html").animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000)
+  })
+}
