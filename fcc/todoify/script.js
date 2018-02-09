815e2575861c1d70b8f2bd814ed824ddf5a7d0a6
0raclewind <look4award@yahoo.com>
2018-02-09 19:52:41 +0000

    added checkbox, timestamp and animations to todoifi

--- fcc/todoify/script.js
+++ fcc/todoify/script.js
@@ -1,10 +1,12 @@
 
 var id = 0;
-
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 function addItem() {
   var input = document.querySelector('#input').value;
   var error = document.getElementById('inputError');
+  var time = new Date();
+  var timeStamp = months[time.getMonth()] + " " + time.getDate() + ", " + time.getHours() + ":" + time.getMinutes();
 
   if (input === "") {
     error.innerHTML = "Your To-Do are so empty...";
@@ -12,9 +14,13 @@ function addItem() {
     error.innerHTML = '';
     var ul = document.querySelector('ul');
     var li = document.createElement('li');
+    var timeDiv = "<div id=timeStamp>Created: " + timeStamp + "</div>";
+    var checkedSpan = '<span class="glyphicon glyphicon-check" onClick=handleCheck(this)></span>';
+    var uncheckedSpan = '<span class="glyphicon glyphicon-unchecked" onClick=handleCheck(this)></span>';
+    var removeSpan = '<span class="glyphicon glyphicon-remove" onClick=removeItem(this) id=' + id + '></span>';
 
-    li.innerHTML = input + '<span class="glyphicon glyphicon-remove" onClick=removeItem(this) id=' + id + '></span>';
-    ul.prepend(li);
+    li.innerHTML = timeDiv + uncheckedSpan + input + removeSpan;
+    $(li).prependTo('.mainList ul').hide().slideDown(400);
     id++;
     document.querySelector('#input').value = "";
   }
@@ -22,7 +28,26 @@ function addItem() {
 
 function removeItem(event) {
   var elem=document.getElementById(event.id).parentNode;
-  elem.parentNode.removeChild(elem);
+  $(elem).slideUp(400);
+  setTimeout(function() {
+    elem.parentNode.removeChild(elem);
+  }, 400);
+}
+
+function handleCheck(event) {
+  var checkName = event.className.split(" ")[1];
+  var eventStyle = event.parentNode.style;
+  if (checkName === "glyphicon-check") {
+    event.className = "glyphicon glyphicon-unchecked";
+    eventStyle.textDecoration = 'none';
+    eventStyle.color = '#eee';
+    eventStyle.backgroundColor = 'rgba(69, 171, 147, 0.5)';
+  } else {
+    event.className = "glyphicon glyphicon-check";
+    eventStyle.textDecoration = 'line-through';
+    eventStyle.color = '#aaa';
+    eventStyle.backgroundColor = 'rgba(36, 110, 92, 0.7)';
+  }
 }
 
 document.getElementById("input")
